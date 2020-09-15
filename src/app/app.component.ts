9e695f1b47beb465e3a3460ba8279ac9f04b5da9
nt <nt@hbt.de>
2020-09-15 12:33:06 +0200

    Added challanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-challanges';
+
+  initChallenge1(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge2(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge3(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge4(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge5(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge6(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge7_1(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge7_2(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge7_3(): void {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge8(): void  {
+    throw new Error('Not Implemented!');
+
+  }
+
+  initChallenge9(): void  {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge10(): void {
+    throw new Error('Not Implemented!');
+  }
+
+  initChallenge11(): void {
+    throw new Error('Not Implemented!');
+  }
 }
